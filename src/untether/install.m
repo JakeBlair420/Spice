321846354a72e03a208370d6b3405d57b94c3096
littlelailo <littlelailo@yahoo.com>
2021-04-24 13:56:24 +0200

    Maybe this is the correct mach_msg offset now :fingers_crossed:

--- src/untether/install.m
+++ src/untether/install.m
@@ -48,7 +48,7 @@ int install(const char *config_path, const char *racoon_path, const char *dyld_c
     myoffsets.new_cache_addr = 0x1c0000000;
     myoffsets.cache_text_seg_size = 0x30000000;
     myoffsets.errno_offset = 0x1b30f1000+0xff8;
-    myoffsets.mach_msg_offset = 0x1f1535018;
+    myoffsets.mach_msg_offset = 0x1f1896018;
     myoffsets.stage2_base = myoffsets.new_cache_addr+myoffsets.cache_text_seg_size+0x4000;
     myoffsets.stage2_max_size = 0x200000;
     myoffsets.thread_max_size = 0x10000;
